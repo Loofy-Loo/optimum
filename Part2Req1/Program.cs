fc25157b48af3f13b08d892d838e46f3638fd2ad
Loofy-Loo <eastwood_478@hotmail.com>
2025-03-30 21:19:58 +0800

    Add files via upload

---
+++ Part2Req1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int Start = 1;
+
+        Console.Write("Enter an integer: ");
+        int End = int.Parse(Console.ReadLine());
+
+        Calculate(Start, End);
+    }
+
+    static void Calculate(int Start, int End)
+    {
+        List<int> numbers = new List<int>();
+        int totalSum = 0;
+
+        for (int num = Start; num <= End; num++)
+        {
+            if (num % 3 == 0 || num % 5 == 0)
+            {
+                numbers.Add(num);
+                totalSum += num;
+            }
+        }
+
+        var ListOfNum = string.Join("+", numbers);
+        Console.WriteLine( ListOfNum + " = " + totalSum);
+    }
+}
